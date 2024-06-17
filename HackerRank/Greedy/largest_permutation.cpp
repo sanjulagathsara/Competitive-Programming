811cf5fb8ba2bcf9d60817a441548263d48edf4f
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2024-06-17 12:46:21 +0530

    largest permutation completed

---
+++ HackerRank/Greedy/largest_permutation.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'largestPermutation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
+vector<int> largestPermutation(int k, vector<int> arr) {
+    int n = arr.size();
+    vector<int> sorted_arr = arr;
+    sort(sorted_arr.begin(),sorted_arr.end(),greater<int>());
+    
+    for(int i=0;i<k;i++){
+        if(i>=(n-1)){
+            break;
+        }
+        
+        //int max = *max_element(arr.begin()+i,arr.end());
+        int max = sorted_arr[i];
+        if(arr[i]!=max){
+            int index = n;
+            for(int j=n-1;j>=i;j--){
+                if(arr[j]==max){
+                    index = j;
+                    break;
+                }
+            }
+            swap(arr[i],arr[index]);
+        }
+        else{
+            if(k<n-1){
+            k++;
+            }
+        }
+    }
+    return arr;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = largestPermutation(k, arr);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
